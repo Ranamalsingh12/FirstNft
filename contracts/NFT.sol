2787fedcba6aef94e65d24e23d4fac88a13ee945
Ranamalsingh12 <ranams99911@gmail.com>
2022-09-28 18:50:30 +0530

    import error resolved

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,12 +4,18 @@
 pragma solidity ^0.8.4;
 
 // Import the openzepplin contracts
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 
 // contract NFTee is an ERC721 contract 
 contract NFTee is ERC721{
     constructor() ERC721("MyFirstNFT","LRN-NFT"){
-
+        // mint 1 NFT to myself
+        _mint(msg.sender,1);
+        _mint(msg.sender,2);
+        _mint(msg.sender,3);
+        _mint(msg.sender,4);
+        _mint(msg.sender,5);
     }
 }
