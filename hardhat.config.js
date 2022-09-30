0f316014b65c98ab6bc327a0b2dbaee81751fe61
Ranamalsingh12 <ranams99911@gmail.com>
2022-09-30 22:23:44 +0530

    starting deployment and setting up environment variable

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+
+  networks: {
+    rinkeby: {
+      url:process.env.ALCHMEY_API_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  }
 };
